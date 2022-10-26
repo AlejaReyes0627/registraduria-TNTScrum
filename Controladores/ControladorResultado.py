d893399e43f202132a9a178c97832812cea3c9a9
hawaslyjose <hawaslyjose@gmail.com>
2022-10-26 10:18:03 -0500

    Se agregaron los comentarios faltantes

--- Controladores/ControladorResultado.py
+++ Controladores/ControladorResultado.py
@@ -1,14 +1,27 @@
+"""
+Importación de las clases Respositorios y Modelos
+"""
+
 from Modelos.Resultado import Resultado
 from Modelos.Mesas import Mesas
 from Modelos.Candidatos import Candidatos
 from Repositorios.RepositorioResultado import RepositorioResultado
 from Repositorios.RepositorioMesas import RepositorioMesas
 from Repositorios.RepositorioCandidatos import RepositorioCandidatos
+
+"""
+Clase controladora que administra los métodos del modelo a la base de datos
+"""
 class ControladorResultado():
     def __init__(self):
         self.repositorioResultado = RepositorioResultado()
         self.repositorioMesas = RepositorioMesas()
         self.repositorioCandidatos = RepositorioCandidatos()
+
+    """
+    Método para hacer un GETALL
+    @return método para encontrar todos los Resultados
+    """
     def index(self):
         return self.repositorioResultado.findAll()
     '''Asignación de mesa y candidato a Resultado'''
@@ -19,6 +32,12 @@ class ControladorResultado():
         nuevoResultado.mesa=laMesa
         nuevoResultado.candidato=elCandidato
         return self.repositorioResultado.save(nuevoResultado)
+
+    """
+    Método que hace un GETBYID 
+    @param id: id de los Resultados impuesto por defecto en mongo
+    @return método para encontrar el Resultado por su Id
+    """
     def show(self,id):
         elResultado=Resultado(self.repositorioResultado.findById(id))
         return elResultado.__dict__
--- Repositorios/RepositorioResultado.py
+++ Repositorios/RepositorioResultado.py
@@ -1,4 +1,11 @@
+"""
+Importación de las clases InterfaceRepositorio Y Modelo Resultados
+"""
 from Repositorios.InterfaceRepositorio import InterfaceRepositorio
 from Modelos.Resultado import Resultado
+
+"""
+Clase repositorio que hereda de la interfaz que lleva a cabo la asignación de la colección y resultados de los JSON
+"""
 class RepositorioResultado(InterfaceRepositorio[Resultado]):
     pass
