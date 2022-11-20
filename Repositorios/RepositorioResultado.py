e8892cda4b57169b7a13ae15fd08698751f87ad1
Alix Ospino <alixhelena@yahoo.es>
2022-11-19 22:25:07 -0500

    voto por candidato y nombre partido

--- Repositorios/RepositorioResultado.py
+++ Repositorios/RepositorioResultado.py
@@ -41,3 +41,34 @@ class RepositorioResultado(InterfaceRepositorio[Resultado]):
         }
         pipeline = [query1,query2]
         return self.queryAggregation(pipeline)
+
+    def getVotosPorCandidatoConPartido(self):
+        query1 = {
+                 '$lookup': {
+                    'from': 'resultados', 
+                    'localField': 'id.candidatos.$id', 
+                    'foreignField': '_id', 
+                    'as': 'result'
+                }
+        }
+        query2 = {
+                '$lookup': {
+                    'from': 'candidatos', 
+                    'localField': 'id.partido.$id', 
+                    'foreignField': '_id', 
+                    'as': 'result2'
+                }
+        }
+        query3 = {
+        '       $match': {
+                    'mesa.$id': ObjectId('63576728e2cd02fa1951a27d')
+                }
+        }
+        query4 = {
+                '$sort': {
+                    'numero_voto': -1
+                }
+        }
+        pipeline = [query1,query2,query3,query4]
+        return self.queryAggregation(pipeline)
+        
